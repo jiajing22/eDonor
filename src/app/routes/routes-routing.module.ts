8bdfee55e22cd795fc6f3992e1d3d9aba12e55d7
jiajing <imjiajing1222@gmail.com>
2023-06-06 10:34:03 +0800

    deploy test

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -3,16 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { startPageGuard } from '@core';
 import { SimpleGuard } from '@delon/auth';
 import { environment } from '@env/environment';
+
+import { LayoutBlankComponent } from '../layout/blank/blank.component';
+import { AuthGuard } from './authGuard';
 import { CallbackComponent } from './passport/callback.component';
+import { ChangePasswordComponent } from './passport/change-password/change-password.component';
 import { UserLockComponent } from './passport/lock/lock.component';
 // passport pages
 import { UserLoginComponent } from './passport/login/login.component';
+import { UserRecoveryComponent } from './passport/recovery/recovery.component';
 import { UserRegisterResultComponent } from './passport/register-result/register-result.component';
 import { UserRegisterComponent } from './passport/register/register.component';
-import {LayoutBlankComponent} from "../layout/blank/blank.component";
-import {UserRecoveryComponent} from "./passport/recovery/recovery.component";
-import {ChangePasswordComponent} from "./passport/change-password/change-password.component";
-import {AuthGuard} from "./authGuard";
 
 const routes: Routes = [
   {
@@ -23,8 +24,9 @@ const routes: Routes = [
       {
         path: 'dashboard',
         loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
-        data: { title: 'Welcome' } },
-      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) },
+        data: { title: 'Welcome' }
+      },
+      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) }
     ]
   },
   {
@@ -36,58 +38,65 @@ const routes: Routes = [
       { path: 'register-result', component: UserRegisterResultComponent, data: { title: 'Register Result' } },
       { path: 'lock', component: UserLockComponent, data: { title: 'Lock' } },
       { path: 'recovery', component: UserRecoveryComponent, data: { title: 'Forget Password' } },
-      { path: 'reset', component: ChangePasswordComponent, data: { title: 'Reset Password' } },
+      { path: 'reset', component: ChangePasswordComponent, data: { title: 'Reset Password' } }
     ]
   },
   { path: 'passport/callback/:type', component: CallbackComponent },
-  { path: 'donorMenu',
-    // canActivate: [AuthGuard],
-    component : LayoutBlankComponent,
+  {
+    path: 'donorMenu',
+    canActivate: [AuthGuard],
+    component: LayoutBlankComponent,
     children: [
       { path: '', redirectTo: 'main', pathMatch: 'full' },
       {
         path: 'main',
         loadChildren: () => import('./dashboard/donor.component/donor-routing.module').then(m => m.DonorRoutingModule),
-        data: { title: 'User Menu' } },
-      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) },
+        data: { title: 'User Menu' }
+      },
+      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) }
     ]
   },
 
-  { path: 'staff',
-    // canActivate: [AuthGuard],
+  {
+    path: 'staff',
+    canActivate: [AuthGuard],
     component: LayoutBlankComponent,
     children: [
       { path: '', redirectTo: 'main', pathMatch: 'full' },
       {
         path: 'main',
         loadChildren: () => import('./dashboard/staff.component/staff-routing.module').then(m => m.StaffRoutingModule),
-        data: { title: 'Staff Menu' } },
-      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) },
-    ]},
-  { path: 'admin',
-    // canActivate: [AuthGuard],
+        data: { title: 'Staff Menu' }
+      },
+      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) }
+    ]
+  },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
     component: LayoutBlankComponent,
     children: [
       { path: '', redirectTo: 'main', pathMatch: 'full' },
       {
         path: 'main',
         loadChildren: () => import('./dashboard/admin.component/admin-routing.module').then(m => m.AdminRoutingModule),
-        data: { title: 'Admin Menu' } },
-      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) },
-    ]},
-  { path: '**', redirectTo: 'exception/404' },
+        data: { title: 'Admin Menu' }
+      },
+      { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) }
+    ]
+  },
+  { path: '**', redirectTo: 'exception/404' }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      routes, {
-        useHash: environment.useHash,
-        // NOTICE: If you use `reuse-tab` component and turn on keepingScroll you can set to `disabled`
-        // Pls refer to https://ng-alain.com/components/reuse-tab
-        scrollPositionRestoration: 'top',
-      }
-    )],
-  exports: [RouterModule],
+    RouterModule.forRoot(routes, {
+      useHash: environment.useHash,
+      // NOTICE: If you use `reuse-tab` component and turn on keepingScroll you can set to `disabled`
+      // Pls refer to https://ng-alain.com/components/reuse-tab
+      scrollPositionRestoration: 'top'
+    })
+  ],
+  exports: [RouterModule]
 })
-export class RouteRoutingModule { }
+export class RouteRoutingModule {}
